c94d6ba9b0ff96e1d96dffdc6ad5d75aaed942d1
Dusty McCord and Eric Settels <dusty+eric@>
2020-02-10 13:41:38 -0800

    clearned quiz.test

--- __tests__/quiz.test.js
+++ __tests__/quiz.test.js
@@ -1,6 +1,4 @@
 import { Quiz } from "../src/quiz";
-import { arrowFunctionExpression } from "@babel/types";
-
 
 describe('Quiz', () => {
   test('should instantiate empty quiz object', () => {
@@ -19,6 +17,7 @@ describe('Quiz', () => {
 let quiz = new Quiz();
 quiz.answers = ["Michael Keaton", "George Clooney", "Val Kilmer"]
 
+//dumby object
 quiz.questions = [
   {
     correct_answer: "Michael Keaton",
@@ -47,44 +46,40 @@ quiz.questions = [
 ];
 
 describe('setScore', () => {
-
-
   test('should update score by 1', () => {
     quiz.setScore();
     expect(quiz.score).toEqual(1);
   });
+});
 
 
-  describe('shuffleAnswers', () => {
-    quiz.shuffleAnswers();
-    test('should check the length of quiz.questions[0].shuffled.length is 4', () => {
-      expect(quiz.questions[0].shuffled.length).toEqual(4);
-    });
-
-    test('should include correct answer in shuffled array', () => {
-      expect(quiz.questions[0].shuffled.includes(quiz.questions[0].correct_answer)).toEqual(true);
-    });
-
-
+describe('shuffleAnswers', () => {
+  quiz.shuffleAnswers();
+  test('should check the length of quiz.questions[0].shuffled.length is 4', () => {
+    expect(quiz.questions[0].shuffled.length).toEqual(4);
   });
 
-  describe('calcPercent', () => {
-    test('Should check that calcPercent function returns a number', () => {
-      expect(typeof quiz.calcPercent()).toEqual("number");
-    });
+  test('should include correct answer in shuffled array', () => {
+    expect(quiz.questions[0].shuffled.includes(quiz.questions[0].correct_answer)).toEqual(true);
+  });
+});
 
-    test('should return the percentage of right answers, 33%', () => {
-      expect(quiz.calcPercent()).toEqual(33)
-    });
+describe('calcPercent', () => {
+  test('Should check that calcPercent function returns a number', () => {
+    expect(typeof quiz.calcPercent()).toEqual("number");
   });
 
-  describe('addAnswers', () => {
-    test('should push argument to quiz.answers array', () => {
-      quiz.addAnswers('Cats');
-      expect(quiz.answers[quiz.answers.length-1]).toEqual('Cats');
-    });
-
+  test('should return the percentage of right answers, 33%', () => {
+    expect(quiz.calcPercent()).toEqual(33)
   });
+});
+
+describe('addAnswers', () => {
+  test('should push argument to quiz.answers array', () => {
+    quiz.addAnswers('Cats');
+    expect(quiz.answers[quiz.answers.length - 1]).toEqual('Cats');
+  });
+
 
 });
 
